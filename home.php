11f7cb10d6d4b7428e0e4534973a9287f558b123
Cem802 <ckirciltepeli@gmail.com>
2024-04-11 21:36:29 +0200

    Add session validation and display user information in home page

--- home.php
+++ home.php
@@ -1,3 +1,11 @@
+<?php
+    session_start();
+
+    include("php/config.php");
+    if(!isset($_SESSION['valid'])){
+        header("Location: index.php");
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,7 +21,22 @@
         </div>
 
         <div class="right-links">
-            <a href="#">Change Profile</a>
+
+            <?php
+
+            $id = $_SESSION['id'];
+            $query = mysqli_query($con, "SELECT * FROM users WHERE Id = '$id'");
+
+            while($result = mysqli_fetch_assoc($query)){
+                $res_Uname = $result['Username'];
+                $res_Email = $result['Email'];
+                $res_Age = $result['Age'];
+                $res_id = $result['Id'];
+            }
+
+            echo "<a href='edit.php?Id=$res_id'>Change Profile</a>"
+
+            ?>
             <a href="logout.php"> <button class="btn">Log Out</button> </a>
         </div>
     </div>
@@ -22,15 +45,15 @@
         <div class="main-box top">
             <div class="top">
                 <div class="box">
-                    <p>Hello <b>User</b>, Welcome</p>
+                    <p>Hello <b><?php echo $res_Uname ?></b>, Welcome</p>
                 </div>
                 <div class="box">
-                    <p>Your email is <b>user@gmail.com</b>.</p>
+                    <p>Your email is <b><?php echo $res_Email ?></b>.</p>
                 </div>
             </div>
             <div class="bottom">
                 <div class="box">
-                    <p>And you are <b>20 years old</b>.</p>
+                    <p>And you are <b><?php echo $res_Age ?> years old</b>.</p>
                 </div>
             </div>
         </div>
